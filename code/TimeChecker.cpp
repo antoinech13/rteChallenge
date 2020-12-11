a87dd700033fc2555e5a586beb77a83c00b2cc82
Laurent CORTIJO <laurent.cortijo@etu.univ-amu.fr>
2020-12-11 15:58:53 +0100

    Merge branch 'main' of https://github.com/antoinech13/rteChallenge into main

---
+++ code/TimeChecker.cpp
@@ -0,0 +1,13 @@
+#include "TimeChecker.h"
+#include "dataCollector.h"
+#include "Interventions.h"
+
+
+TimeChecker::TimeChecker(dataCollector data) {
+	this->interventions = data.getInterventions();
+	this->deltas = data.getDelta();
+	
+
+}
+
+TimeChecker::TimeChecker() {}
---
+++ code/TimeChecker.h
@@ -0,0 +1,17 @@
+#include "dataCollector.h"
+#include "Interventions.h"
+
+
+class TimeChecker 
+
+
+	public:
+	TimeChecker(dataCollector data);
+	TimeChecker();
+
+
+
+
+
+	private:
+};
