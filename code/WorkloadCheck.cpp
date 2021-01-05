b20264a8dc4ef1d11669f1c631f8a6e18f894485
antoine <antoine-spartan@hotmail.fr>
2021-01-05 21:43:12 +0100

    bn

--- code/WorkloadCheck.cpp
+++ code/WorkloadCheck.cpp
@@ -8,26 +8,39 @@ WorkloadCheck::WorkloadCheck(dataCollector data) {
 	this->interventions = data.getInterventions();
 	this->ressouces = data.getResources();
 	vector<int> tab(data.getT(), 0);
-	
+
 };
 
 WorkloadCheck::WorkloadCheck() {}
 
 void WorkloadCheck::workloadCheckMax(vector<int> soluce) {
 	int jour, delta, delaisjour, nomC, timeIdx, startingTime;
-	
+
 	for (int indice = 0; indice < soluce.size(); indice++) {
 		jour = soluce[indice] - 1;
 		delta = this->interventions[indice].getDelta()[jour];
 		vector<pair<int, vector<pair<int, vector<pair<int, double>>>>>> workload = this->interventions[indice].getWorkload();
 		for (int nbjour = 0; nbjour < delta; nbjour++) {
 			for (int c = 0; c < workload.size(); c++) {
-				int nomC = workload[c].first;
-				int delaisjour = jour + nbjour;
-				arrayWorkload[nomC][delaisjour] += workload[c].second[delaisjour].second[jour].second;
-	
+				nomC = workload[c].first;
+				delaisjour = jour + nbjour;
+				//cout << "soluce: " << soluce[indice] << '\n';
+				//cout << "delta: " << delta << '\n';
+				//cout << "nomC: " << nomC << '\n';
+				//cout << "delai: " << delaisjour << '\n';
+				//cout << "size1: " << arrayWorkload.size() << " size2: " << arrayWorkload[0].size() << '\n';
+				//cout << "test?: " << '\n';
+				//cout << "Intervention ID: " << this->interventions[indice].getInterId() << '\n';
+				timeIdx = timeToIdx(delaisjour + 1, workload[c].second);
+
+				if (timeIdx != -1)
+					startingTime = startingTimeToIdx(jour + 1, workload[c].second[timeIdx].second);
+
+				if (timeIdx != -1 && startingTime != -1)
+					arrayWorkload[nomC][delaisjour] += workload[c].second[timeIdx].second[startingTime].second;
+
 				if (arrayWorkload[nomC][delaisjour] > this->ressouces[nomC].second["max"][delaisjour]) {
-					
+
 					this->interventionBad.push_back(this->interventions[indice].getInterId());
 				}
 			}
@@ -69,4 +82,20 @@ vector<vector<int>> WorkloadCheck::getTimeBad() {
 
 vector<vector<int>> WorkloadCheck::getArrayWorkload() {
 	return arrayWorkload;
+}
+
+int WorkloadCheck::timeToIdx(int time, vector<pair<int, vector<pair<int, double>>>> workloadTime) {
+	for (int i = 0; i < workloadTime.size(); i++)
+		if (time == workloadTime[i].first)
+			return i;
+	//cout << "problem with WorkloadCheck: Time step not found! time: " << time << '\n';
+	return -1;
+}
+
+int WorkloadCheck::startingTimeToIdx(int time, vector<pair<int, double>> workloadSTime) {
+	for (int i = 0; i < workloadSTime.size(); i++)
+		if (time == workloadSTime[i].first)
+			return i;
+	//cout << "problem with WorkloadCheck: Starting time step not found! time: " << time << '\n';
+	return -1;
 }
